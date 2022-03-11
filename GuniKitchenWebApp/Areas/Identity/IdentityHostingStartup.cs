e63ce94ef4ea5dff05de9a8b2124a33992cb6da7
Bhavya Vyas <bhavy.vyas.11@gmail.com>
2022-03-11 11:08:36 +0530

    Added the Scaffolded and Customized the Identity User and Identity Role Pages.

---
+++ GuniKitchenWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GuniKitchenWebApp.Data;
+using GuniKitchenWebApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GuniKitchenWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace GuniKitchenWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
